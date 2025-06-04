a00e15c9a12c65463eb051b67307075c91b23972
javi <javier.gijon02@gmail.com>
2025-06-04 20:34:35 +0200

    setup: Se crea estrucutura de proyecto usando bazel como gestor de dependencias y se añade pruba de funcionamiento de vertx

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+public class Main extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        // Crear un servidor HTTP en el puerto 8080
+        vertx.createHttpServer()
+                .requestHandler(req -> {
+                    // Responder con "Hello, World!"
+                    req.response().end("Hello, World!");
+                })
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        System.out.println("Servidor iniciado en el puerto 8080");
+                    } else {
+                        System.err.println("Error al iniciar el servidor: " + result.cause());
+                    }
+                });
+    }
+
+    public static void main(String[] args) {
+        // Crear un Vertx instance y desplegar el verticle
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new Main());
+    }
+}
