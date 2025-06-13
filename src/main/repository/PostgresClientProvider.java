6014963a55baa51d2b3f3543d74c22241c2a386b
JaviFanjul <javier.gijon02@gmail.com>
2025-06-13 10:05:21 +0200

    feat: Se referencia .env y se evita hardcodear las credenciales del servidor

--- src/main/repository/PostgresClientProvider.java
+++ src/main/repository/PostgresClientProvider.java
@@ -5,17 +5,19 @@ import io.vertx.sqlclient.PoolOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.core.Vertx;
 import io.vertx.sqlclient.SqlClient;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class PostgresClientProvider {
 
+    private static final Dotenv dotenv = Dotenv.load();
     // Conection to the SQL Server (locally)
     public static SqlClient createClient(Vertx vertx) {
         PgConnectOptions connectOptions = new PgConnectOptions()
-                .setPort(5432)
-                .setHost("localhost")
-                .setDatabase("pokemon_db")
-                .setUser("postgres")
-                .setPassword("postgres");
+                .setPort(Integer.parseInt(dotenv.get("DB_PORT")))
+                .setHost(dotenv.get("DB_HOST"))
+                .setDatabase(dotenv.get("DB_NAME"))
+                .setUser(dotenv.get("DB_USER"))
+                .setPassword(dotenv.get("DB_PASSWORD"));
 
         // Pool of size 5 to prevent too many requests
         PoolOptions poolOptions = new PoolOptions().setMaxSize(5);
